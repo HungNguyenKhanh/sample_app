64f75a96ac3473be9a5ade2a1083cd5b9837b250
Hung Nguyen Khanh <hungnk1995@gmail.com>
2018-07-10 00:29:17 +0700

    Chapter 10: Updating, showing, and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
-    flash[:danger] = t ".not_exist"
-    redirect_to root_path
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :get_user, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.profile.paginate(per_page: Settings.users_page.SIZE,
+      page: params[:page])
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -21,9 +26,49 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t ".deleted"
+    redirect_to users_url
+  end
+
   private
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "login_req"
+    redirect_to login_url
+  end
+
+  def get_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t ".not_exist"
+    redirect_to root_path
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user? @user
+  end
+
   def user_params
-    params.require(:user).permit :name, :email, :password,
-      :password_confirmation
+    params.require(:user).permit :name, :email,
+      :password, :password_confirmation
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
