db000064b6759978faee98ff8615fb1f460f4f0e
Hung Nguyen Khanh <hungnk1995@gmail.com>
2018-07-10 00:29:17 +0700

    Chapter 11: Account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: sess_param[:email].downcase
     if user && user.authenticate(sess_param[:password])
       log_in user
-      sess_param[:remember_me] == "1" ? remember(user) : forget(user)
+      check_remember user
       redirect_to user
     else
       flash[:danger] = t ".invalid"
@@ -17,5 +17,11 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def check_remember user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
   end
 end
