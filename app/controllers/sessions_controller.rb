63bffce4c9d9432f471101a4d2bbdfd8436b0eef
Hung Nguyen Khanh <hungnk1995@gmail.com>
2018-07-10 00:29:17 +0700

    Chapter 12: Password reset

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,14 @@ class SessionsController < ApplicationController
     sess_param = params[:session]
     user = User.find_by email: sess_param[:email].downcase
     if user && user.authenticate(sess_param[:password])
-      log_in user
-      check_remember user
-      redirect_to user
+      if user.activated?
+        log_in user
+        check_remember user
+        redirect_back_or user
+      else
+        flash[:warning] = t ".chk_activate"
+        redirect_to root_url
+      end
     else
       flash[:danger] = t ".invalid"
       render :new
